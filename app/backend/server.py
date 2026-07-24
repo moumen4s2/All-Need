e09a6b18e4d91500838a2aa7327dbc2242dd13b4
moumen4s2 <moumenabdalmohsen714@gmail.com>
2026-07-25 01:52:24 +0300

    Fix indentation

--- app/backend/server.py
+++ app/backend/server.py
@@ -81,7 +81,7 @@ class AdminLogin(BaseModel):
     password: str
 
 class UpdateOrderStatus(BaseModel):
-status: str
+    status: str
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / ".env")
