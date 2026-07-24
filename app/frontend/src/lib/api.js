799f75c331503122b0c8f3327975c2e938e37ff3
moumen4s2 <moumenabdalmohsen714@gmail.com>
2026-07-24 16:20:21 +0300

    Fix CORS configuration

--- app/frontend/src/lib/api.js
+++ app/frontend/src/lib/api.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API = `${BACKEND_URL}/api`;
 
+console.log("BACKEND_URL =", BACKEND_URL);
+console.log("API =", API);
+
 export const api = axios.create({
   baseURL: API,
   withCredentials: true,
