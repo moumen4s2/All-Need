70b5ca7e18cbc4f162f9b9e13d80fb91ac29a8a0
moumen4s2 <moumenabdalmohsen714@gmail.com>
2026-08-08 15:46:39 +0300

    change the database to postgreSQL

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,19 +1,43 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./allneeds.db"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
+
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1
+    )
+
+elif DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql+asyncpg://",
+        1
+    )
+
 
 engine = create_async_engine(
     DATABASE_URL,
     echo=False,
-    future=True
+    pool_pre_ping=True
 )
+
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
+
 
 Base = declarative_base()
 
